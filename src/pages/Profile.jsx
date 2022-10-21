fa1be956df65fc60bb931f612c842cfc1b359f09
sebastian morgado <sebaah89@gmail.com>
2022-10-21 01:02:42 -0300

    update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -33,6 +33,86 @@ export function Profile() {
     if (!user) return null;
 
     return (
-        <div>profile page</div>
+        <>
+            <div>
+                <h1>Profile</h1>
+                <div>
+                    <h2>Preview</h2>
+                    <div>
+                        <div>
+                            <img src={user.avatar} alt="avatar" />
+                        </div>
+                        <div>
+                            <p>About {user.userName}</p>
+                            <p>{user.followers} followers</p>
+                            {user.about && user.about.trim() !== "" ? // if user.about is not empty
+                                <p>{user.about}</p> 
+                                    : 
+                                <p>Hello world!, im {user.userName}.</p>
+                            }
+                        </div>
+                    </div>
+
+                </div>
+
+                <div>
+                    <h2>Profile Settings</h2>
+                    <div>
+                        <p>Username:</p>
+                        <input type="text" value={user.userName} />
+                    </div>
+                    <div>
+                        <p>Public username:</p>
+                        <input type="text" value={user.streamData.name} />
+                    </div>
+                    <div>
+                        <p>About Me:</p>
+                        <textarea value={user.about} />
+                    </div>
+                    
+                    <button>
+                        Save Changes
+                    </button>
+                </div>
+                <div>
+                    <h2>Contact</h2>
+                    <div>
+                        <h3>Email:</h3>
+                        <input type="text" value={user.email} />
+                    </div>
+                    <div>
+                        <h3>Password:</h3>
+                        <p>Current Password</p>
+                        <input type="password" placeholder="*******" />
+
+                        <p>New Password</p>
+                        <input type="password" placeholder="*******" />
+                    </div>
+                    <button>
+                        Save Changes
+                    </button>
+                </div>
+                <div>
+                    <h2>Profile Image</h2>
+                    <img src={user.profileImage} alt="profile" />
+                </div>
+                <div>
+                    <h2>Profile Banner</h2>
+                    <img src={user.profileBanner} alt="banner" />
+                </div>
+                <h1>Stream Settings</h1>
+                <div>
+                    <p>Stream Key</p>
+                    <div>
+                        <input type="password" value={user.key} />
+                    </div>
+                </div>
+                <div>
+                    <button>
+                        Delete Account
+                    </button>
+                </div>
+            </div>
+        </>
     )
 }
--- src/services/User.service.js
+++ src/services/User.service.js
@@ -1,4 +1,4 @@
-import { get } from '../utils/httpClient';
+import { get, post, del } from '../utils/httpClient';
 
 const API_URL = "/user";
 
@@ -6,17 +6,28 @@ const getProfile = () => {
     return get(API_URL + "/profile");
 }
 
+const getStreamDetails = () => {
+    return get(API_URL + "/stream");
+}
+
+const updateProfile = (data) => {
+    return post(API_URL + "/profile", data);
+}
+
+const updateStreamDetails = (data) => {
+    return post(API_URL + "/stream", data);
+}
+
+const deleteAccount = () => {
+    return del(API_URL + "/delete");
+}
+
 const UserService = {
     getProfile,
+    getStreamDetails,
+    updateProfile,
+    updateStreamDetails,
+    deleteAccount
 };
-
-/*
-profile
-stream
-profile update
-stream update
-delete account
-*/
-
 
 export default UserService;
