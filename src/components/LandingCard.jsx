10a40000dea87a6c4f865a35a5f27fbdb0d35447
Anton <antonyrodriguezc.2001@gmail.com>
2022-11-19 21:38:34 -0300

    update

--- src/components/LandingCard.jsx
+++ src/components/LandingCard.jsx
@@ -1,25 +1,29 @@
-import styles from './LandingCard.module.scss';
+import styles from "./LandingCard.module.scss";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export function LandingCard({ stream }) {
+  const { username, avatar, banner } = stream;
 
-    const { username, avatar, banner } = stream;
-
-    return (
-        <Link to={`/stream/${username}`} className={styles.streamerCard} /*style={{ backgroundImage: `url(${banner})`}}*/>
-            
-            <div className={styles.streamerBackgroundImage}>
-                <img src={banner} alt={username} />
-            </div>
-            <div className={styles.streamImage}>
-                <Link to={`/stream/${stream.username}`}>
-                    <figure className={styles.imageBorder}>
-                        <img src={avatar} alt={username} />
-                    </figure>
-                </Link>
-            </div>
-            <h3>{username}</h3>
-        </Link>
-    )
+  return (
+    <Link
+      to={`/stream/${username}`}
+      className={
+        styles.streamerCard
+      } /*style={{ backgroundImage: `url(${banner})`}}*/
+    >
+      <div className={styles.streamerBackgroundImage}>
+        <img src={banner} alt={username} />
+        <div className={styles.streamImage}>
+          <Link to={`/stream/${stream.username}`}>
+            <figure className={styles.imageBorder}>
+              <img src={avatar} alt={username} />
+            </figure>
+          </Link>
+        </div>
+        <h3>{username}</h3>
+      </div>
+      
+    </Link>
+  );
 }
