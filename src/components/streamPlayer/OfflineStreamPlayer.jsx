391a6cf6a3e27324145f2fec217a96cbce37073e
RobC515 <robertocontreras515@gmail.com>
2022-11-29 11:19:18 -0300

    Update

--- src/components/streamPlayer/OfflineStreamPlayer.jsx
+++ src/components/streamPlayer/OfflineStreamPlayer.jsx
@@ -1,43 +1,58 @@
 import styles from './OfflineStreamPlayer.module.scss';
 import { Link } from "react-router-dom";
+import UserService from "../../services/User.service";
 
 import { getImg } from '../../utils/httpClient';
+import { useEffect, useState } from 'react';
 
 export function OfflineStreamPlayer({ stream }) {
+    const [tipo, setTipo] = useState(null);
+    const [avatar, setAvatar] = useState(null);
+    const [banner, setBanner] = useState(null);
 
-    const avatar = getImg("avatar", stream.username);
+    useEffect(() => {
+        UserService.getUserAvatar(stream.username)
+            .then(data => {
+                setAvatar(data);
+            })
+    }, [stream.username]);
+
+    useEffect(() => {
+        UserService.getUserBanner(stream.username)
+            .then(data => {
+                setBanner(data);
+            })
+    }, [stream.username]);
+
+
 
     return (
         <div className={styles.streamRoot}>
-            <div className={styles.streamVideo}>
-                
+            <div className={styles.streamFrontPageImage}>
+                <Link to={`/stream/${stream.username}`}>
+                    <figure className={styles.imageBorderFrontPage}>
+                        <img src={banner} alt={stream.title} />
+                    </figure>
+                </Link>
             </div>
-
-            <div className={styles.streamDetails}>
-                <div className={styles.streamImage}>
-                    <Link to={`/stream/${stream.username}`}>
-                        <figure className={styles.imageBorder}>
-                            <img src={avatar} alt={stream.title} />
-                        </figure>
-                    </Link>
-                </div>
-                <div className={styles.streamInfo}>
-                    <p className={styles.streamUsername}>{stream.username}</p>
-                    <p className={styles.streamTitle}>{stream.title}</p>
-                    <div>
-                        <p className={`${styles.streamCategory}`}>{stream.category}</p>
-                        {stream.tags.length > 0 && (
-                            <div className={styles.streamTags}>
-                                {stream.tags.map((tag, index) =>
-                                    <div className={styles.tag} key={index}>
-                                        <Link to={`/search/tag/${tag}`} className={styles.tagUrl}>
-                                            <span>{tag}</span>
-                                        </Link>
-                                    </div>
-                                )}
-                            </div>
-                        )}
+            <div className={styles.heroContainer}>
+                <div className={styles.streamDetails}>
+                    <div className={styles.streamImage}>
+                        <Link to={`/stream/${stream.username}`}>
+                            <figure className={styles.imageBorder}>
+                                <img src={avatar} alt={stream.title} />
+                            </figure>
+                        </Link>
+                        <div className={styles.streamUsername}>
+                            <h1 >{stream.username}</h1>
+                            <h4 >Followers {stream.followers}</h4>
+                        </div>
                     </div>
+                    <div>
+                        <h2 className={styles.streamAbout}>About {stream.username}</h2>
+                        <p className={styles.streamDescriptionAbout}>{stream.about}</p>
+                    </div>
+
                 </div>
             </div>
         </div>
--- src/components/streamPlayer/OnlineStreamPlayer.jsx
+++ src/components/streamPlayer/OnlineStreamPlayer.jsx
@@ -1,44 +1,69 @@
 import styles from './OnlineStreamPlayer.module.scss';
 import { Link } from "react-router-dom";
 import { VideoPlayer } from '../VideoPlayer';
+import UserService from "../../services/User.service";
+import { useEffect, useState } from 'react';
 
 import { getImg } from '../../utils/httpClient';
 
 export function OnlineStreamPlayer({ stream }) {
 
-    const avatar = getImg("avatar", stream.username);
+    const [avatar, setAvatar] = useState(null);
+
+    useEffect(() => {
+        UserService.getUserAvatar(stream.username)
+            .then(data => {
+                setAvatar(data);
+            })
+    }, [stream.username]);
 
     return (
         <div className={styles.streamRoot}>
             <div className={styles.streamVideo}>
                 <VideoPlayer streamUrl={stream.url} />
             </div>
-
-            <div className={styles.streamDetails}>
-                <div className={styles.streamImage}>
-                    <Link to={`/stream/${stream.username}`}>
-                        <figure className={styles.imageBorder}>
-                            <img src={avatar} alt={stream.title} />
-                        </figure>
-                    </Link>
-                </div>
-                <div className={styles.streamInfo}>
-                    <p className={styles.streamUsername}>{stream.username}</p>
-                    <p className={styles.streamTitle}>{stream.title}</p>
-                    <div>
-                        <p className={`${styles.streamCategory}`}>{stream.category}</p>
-                        {stream.tags.length > 0 && (
-                            <div className={styles.tagsContainer}>
-                                {stream.tags.map((tag, index) =>
-                                    <div className={styles.tag} key={index}>
-                                        <Link to={`/search/tag/${tag}`} className={styles.tagUrl}>
-                                            <span>{tag}</span>
-                                        </Link>
-                                    </div>
-                                )}
+            <div className={styles.heroContainer}>
+                <div className={styles.heroContainerLeft}>
+                    <div className={styles.streamDetails}>
+                        <div className={styles.streamImage}>
+                            <Link to={`/stream/${stream.username}`}>
+                                <figure className={styles.imageBorder}>
+                                    <img src={avatar} alt={stream.title} />
+                                </figure>
+                            </Link>
+                            <div className={styles.streamInfo}>
+                                <h1 className={styles.streamUsername}>{stream.username}</h1>
+                                <h3 className={styles.streamTitle}>{stream.title}</h3>
+                                <div>
+                                    <p className={`${styles.streamCategory}`}>{stream.category}</p>
+                                    {stream.tags.length > 0 && (
+                                        <div className={styles.tagsContainer}>
+                                            {stream.tags.map((tag, index) =>
+                                                <div className={styles.tag} key={index}>
+                                                    <Link to={`/search/tag/${tag}`} className={styles.tagUrl}>
+                                                        <span>{tag}</span>
+                                                    </Link>
+                                                </div>
+                                            )}
+                                        </div>
+                                    )}
+                                </div>
                             </div>
-                        )}
+                        </div>
+
                     </div>
+                </div>
+                <div className={styles.heroContainerRight}>
+                    <div className={styles.signupBox}>
+                        <h2 className={styles.buttonText}>Follow</h2>
+                    </div>
+                </div>
+
+            </div>
+            <div>
+                <div>
+                    <h2 className={styles.streamAbout}>About {stream.username}</h2>
+                    <p className={styles.streamDescriptionAbout}>{stream.about}</p>
                 </div>
             </div>
         </div>
